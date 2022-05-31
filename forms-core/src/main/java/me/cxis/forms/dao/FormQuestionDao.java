b1d7fcf3a976a9f1b77752b09f09dfc9d3de1d3f
Pullock <pullocked@gmail.com>
2022-06-01 00:51:55 +0800

    表单创建

---
+++ forms-core/src/main/java/me/cxis/forms/dao/FormQuestionDao.java
@@ -0,0 +1,27 @@
+package me.cxis.forms.dao;
+
+import me.cxis.forms.dao.mapper.FormQuestionDOMapper;
+import me.cxis.forms.dao.model.FormQuestionDO;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class FormQuestionDao {
+
+    @Resource
+    private FormQuestionDOMapper formQuestionDOMapper;
+
+    public void batchSave(List<FormQuestionDO> questions) {
+        LocalDateTime now = LocalDateTime.now();
+        questions.forEach(question -> {
+            question.setIsDeleted(false);
+            question.setCreateTime(now);
+            question.setUpdateTime(now);
+        });
+
+        formQuestionDOMapper.insertBatch(questions);
+    }
+}
