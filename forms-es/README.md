9e1e7b3280a29d279f6e604a187ebe30fb110231
Pullock <pullocked@gmail.com>
2022-06-10 00:47:47 +0800

    es

--- forms-es/README.md
+++ forms-es/README.md
@@ -15,3 +15,24 @@ bin/elasticsearch-reset-password --username elastic -i
 ```
 帐号密码：
 - elastic/12345678
+
+重置Kibana的enrollment token：
+
+```
+docker exec -it {elastic_search_container_id} /bin/bash
+bin/elasticsearch-create-enrollment-token --scope kibana
+```
+
+获取Verification code：
+
+```
+docker exec -it {kibana_container_id} /bin/bash
+bin/kibana-verification-code
+```
+
+## 启动Kibana
+
+```
+docker pull docker.elastic.co/kibana/kibana:8.2.2
+docker run --name kibana --net elastic -p 5601:5601 docker.elastic.co/kibana/kibana:8.2.2
+```
---
+++ forms-es/src/main/java/me/cxis/forms/es/ESClient.java
@@ -0,0 +1,37 @@
+package me.cxis.forms.es;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class ESClient {
+
+    private RestClient restClient;
+
+    private ElasticsearchTransport transport;
+
+    private ElasticsearchClient client;
+
+    @PostConstruct
+    private void init() {
+        restClient = RestClient.builder(
+                new HttpHost("localhost", 9200)
+        ).build();
+
+        transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+
+        client = new ElasticsearchClient(transport);
+    }
+
+    public void search() {
+        SearchResponse<>
+    }
+}
